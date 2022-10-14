79ac9850d6c59cc7fc770a1db78801422863a113
Xotelkin <stefanno666@mail.ru>
2022-10-14 17:10:12 +0200

    Добавил код задания

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string[] array1 = 
+{
+    "МИР",
+    "Дружба",
+    "Жвачка"
+};
+
+int size = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length < 4)
+    {
+        size++;
+    }
+}
+string[] array2 = new string[size];
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length < 4)
+    {
+        array2[count] = array1[i];
+        count++;
+    }
+}
+
+Console.Write("[{0}]", string.Join(", ", array2));
+
+
